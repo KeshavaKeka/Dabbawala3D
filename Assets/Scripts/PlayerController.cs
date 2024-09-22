c7fdabefcfe750af12889ccae382989e2158d9cb
KeshavaKeka <106547733+KeshavaKeka@users.noreply.github.com>
2024-09-22 09:01:44 +0530

    urban done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Objective")]
-    public float loc1 = 6;
-    public float loc2 = 6;
-    public float loc3 = 8;
+    public int loc1 = 3;
+    public int loc2 = 5;
+    public int loc3 = 5;
+    public int loc4 = 7;
     [Header("Player Settings")]
-    public float driftFactor = 0.3f; // Not as applicable in 3D but can be tweaked for lateral friction
-    public float accelerationFactor = 5f;
-    public float turnFactor = 5f;
-    public float maxSpeed = 10f;
-    public float capacity;
+    public float driftFactor = 0.3f;
+    public float accelerationFactor = 1.5f;
+    public float turnFactor = 120f;
+    public float maxSpeed = 3f;
+    public int capacity = 5;
     float accelerationInput = 0;
     float steeringInput = 0;
-
+    private GameManagerUrban gmu;
+    private GameObject firstPrefabInstance1;
+    private GameObject firstPrefabInstance2;
+    private GameObject firstPrefabInstance3;
+    private GameObject firstPrefabInstance4;
+    private GameObject secondPrefabInstance;
+    private int content;
+    public TextMeshProUGUI obj;
+    public TextMeshProUGUI cont;
     float rotationAngle = 0;
 
     float velocityVsUp = 0;
 
     Rigidbody rb;
+
+    SPUrban sp;
 
     void Awake()
     {
@@ -31,13 +43,47 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        content = 0;
+        sp = GameObject.Find("SpawnPositionUrban").GetComponent<SPUrban>();
+        gmu = GameObject.Find("GameManagerUrban").GetComponent<GameManagerUrban>();
+        obj.text = $"Pending - \nLoc1: {loc1}\nLoc2: {loc2}\nLoc3: {loc3}\nLoc4: {loc4}";
+        cont.text = $"Dabbas: {content}\nCapacity: {capacity}";
+        if (sp!= null)
+        {
+            if (sp.firstPrefabInstances.Count > 0)
+            {
+                firstPrefabInstance1 = sp.firstPrefabInstances[0];
+                firstPrefabInstance2 = sp.firstPrefabInstances[1];
+                firstPrefabInstance3 = sp.firstPrefabInstances[2];
+                firstPrefabInstance4 = sp.firstPrefabInstances[3];
+            }
+            secondPrefabInstance = sp.secondPrefabInstance;
+        }
+        else
+        {
+            Debug.LogWarning("SPUrban script not found or not assigned.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(loc1+loc2+loc3+loc4 == 0)
+        {
+            gmu.GameOver();
+        }
+        if(loc1 + loc2 + loc3 + loc4 <= 17)
+        {
+            capacity = 7;
+            maxSpeed = 5f;
+            turnFactor = 180;
+        }
+        if (loc1 + loc2 + loc3 + loc4 <= 10)
+        {
+            capacity = 10;
+            maxSpeed = 7f;
+            turnFactor = 210;
+        }
     }
 
     void FixedUpdate()
@@ -110,6 +156,75 @@ public class PlayerController : MonoBehaviour
         else if(other.gameObject.CompareTag("delivery"))
         {
             Debug.Log("Delivery");
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Pressed E");
+            if (other.gameObject.CompareTag("delivery"))
+            {
+                if (other.gameObject == firstPrefabInstance1)
+                {
+                    if (loc1 > content)
+                    {
+                        content = 0;
+                        loc1 -= content;
+                    }
+                    else
+                    {
+                        content -= loc1;
+                        loc1 = 0;
+                    }
+                }
+                else if (other.gameObject == firstPrefabInstance2)
+                {
+                    if (loc2 > content)
+                    {
+                        content = 0;
+                        loc2 -= content;
+                    }
+                    else
+                    {
+                        content -= loc2;
+                        loc2 = 0;
+                    }
+                }
+                else if (other.gameObject == firstPrefabInstance3)
+                {
+                    if (loc3 > content)
+                    {
+                        content = 0;
+                        loc3 -= content;
+                    }
+                    else
+                    {
+                        content -= loc3;
+                        loc3 = 0;
+                    }
+                }
+                else
+                {
+                    if (loc4 > content)
+                    {
+                        content = 0;
+                        loc4 -= content;
+                    }
+                    else
+                    {
+                        content -= loc4;
+                        loc4 = 0;
+                    }
+                }
+            }
+            else if(other.gameObject.CompareTag("dabbas"))
+            {
+                content += (capacity - content);
+            }
+            obj.text = $"Pending - \nLoc1: {loc1}\nLoc2: {loc2}\nLoc3: {loc3}\nLoc4: {loc4}";
+            cont.text = $"Dabbas: {content}\nCapacity: {capacity}";
         }
     }
 }
